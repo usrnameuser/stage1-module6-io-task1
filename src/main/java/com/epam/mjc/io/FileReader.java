d886c55550dda209a30cca6066c0c2b81c0a7c02
usrnameuser <150504703+usrnameuser@users.noreply.github.com>
2024-06-14 12:48:30 +0300

    Update FileReader.java

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -7,7 +7,7 @@ import java.io.FileNotFoundException;
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        File profile = new File("src/main/resources/Profile");
+        File profile = new File("src/main/resources/Profile.txt");
         try {
             FileInputStream profileInputStream = new FileInputStream(profile);
         }
