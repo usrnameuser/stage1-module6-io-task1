b5dfcb9b94305a8af8384eb9871e2b4e4a4d6a2a
usrnameuser <150504703+usrnameuser@users.noreply.github.com>
2024-06-14 11:43:02 +0300

    Update FileReader.java

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -6,7 +6,7 @@ import java.io.FileInputStream;
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        FileInputStream profileInputStream = new FileInputStream("src/main/java/com/epam/mjc/resourcesс/Profile.txt");
+        FileInputStream profileInputStream = new FileInputStream("/resourcesс/Profile.txt");
         return new Profile();
     }
 }
