c39fd2e4f20537fce662d3c4d1c3ef52c90abb65
usrnameuser <150504703+usrnameuser@users.noreply.github.com>
2024-06-14 14:21:29 +0300

    Update FileReader.java

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -13,7 +13,6 @@ public class FileReader {
             FileInputStream profileInputStream = new FileInputStream(profile.listFiles()[0]);
         }
         catch (FileNotFoundException e) {
-            e.printStackTrace();
         }
         return new Profile(profile.listFiles()[0].toString(), 0, new String(), new Long(0));
     }
