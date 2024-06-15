6486a889440e38af1c3426aa03131d358167084a
usrnameuser <150504703+usrnameuser@users.noreply.github.com>
2024-06-15 05:09:59 +0300

    Update FileReader.java

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -3,17 +3,19 @@ package com.epam.mjc.io;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
         //File profile = new File("src/main/resources/Profile.txt");
         File profile = new File("src/main/resources");
-        try {
+        /*try {
             FileInputStream profileInputStream = new FileInputStream(profile.listFiles()[0]);
         }
         catch (FileNotFoundException e) {
-        }
+        }*/
+        FileReader profileReader = new FileReader(profile.listFiles()[0]);
         return new Profile(profile.listFiles()[0].toString(), 0, new String(), new Long(0));
     }
 }
