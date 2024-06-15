7d168f75538620fbe280e8120212145dd2190d96
usrnameuser <150504703+usrnameuser@users.noreply.github.com>
2024-06-15 11:08:27 +0300

    Update FileReader.java

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -7,16 +7,16 @@ import java.io.FileNotFoundException;
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        //File profile = new File("src/main/resources/Profile.txt");
-        File profile = new File("src/main/resources");
-        FileNotFoundException ex;
-        try {
+        File profile = new File("src/main/resources/Profile.txt");
+
+        /*try {
             FileInputStream profileInputStream = new FileInputStream(profile.listFiles()[0]);
         }
         catch (FileNotFoundException e) {
-            ex = e;
-        }
-
-        return new Profile(ex.toString(), 0, ex.toString(), new Long(0));
+        }*/
+        Boolean b1 = profile.isFile();
+        Boolean b3 = profile.isDirectory();
+        
+        return new Profile(b1.toString(), 0, b3.toString(), new Long(0));
     }
 }
